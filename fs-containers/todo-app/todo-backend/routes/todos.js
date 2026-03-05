755458563a15ca9e3c32eed47eaf9f7892933af3
adams <108461931+AdamsFontil@users.noreply.github.com>
2026-03-04 21:41:03 -0500

    12.7ish put route can be improved

--- fs-containers/todo-app/todo-backend/routes/todos.js
+++ fs-containers/todo-app/todo-backend/routes/todos.js
@@ -21,6 +21,9 @@ const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
+  console.log('id--midware', id)
+  console.log('midware')
+  // console.log('req from middleware', req)
   req.todo = await Todo.findById(id)
   if (!req.todo) return res.sendStatus(404)
 
@@ -29,19 +32,49 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete()
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  // console.log('what is req', req.todo)
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const { text, done } = req.body
+  console.log(`here is text-${text}, and done-${done}`)
+  req.todo.text = text
+  req.todo.done = done
+  req.todo.save()
+  console.log('Newtodo??', req.todo)
+  res.send(req.todo)
 });
+
+
+
+// notesRouter.put('/:id', (request, response, next) => {
+//   const { content, important } = request.body
+
+//   Note.findById(request.params.id)
+//     .then(note => {
+//       if (!note) {
+//         return response.status(404).end()
+//       }
+
+//       note.content = content
+//       note.important = important
+
+//       return note.save().then(updatedNote => {
+//         response.json(updatedNote)
+//       })
+//     })
+//     .catch(error => next(error))
+// })
+
+
 
 router.use('/:id', findByIdMiddleware, singleRouter)
 
